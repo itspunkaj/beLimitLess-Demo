dcc5631ff06a6c6c1b33eba6e53ac5ff97e8c8c0
Pankaj Singh <pankaj.s@launchxlabs.ai>
2024-11-08 15:15:42 +0530

    Add Icons to the actionbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { SwiperSlide } from 'swiper/react'
 import { Pagination } from 'swiper/modules';
+import { MdCall } from 'react-icons/md'
+import { FaNoteSticky, FaPlus } from 'react-icons/fa6'
+import { FaSearch, FaShareSquare } from 'react-icons/fa'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,34 +23,33 @@ function App() {
             <img src={beLL} className='h-8' />
           </div>
           <div className='rounded-full overflow-hidden border-2 border-primaryPink'>
-            <img className='h-8' src="https://pics.craiyon.com/2023-11-26/oMNPpACzTtO5OVERUZwh3Q.webp" />
+            <img className='h-8 ' src="https://pics.craiyon.com/2023-11-26/oMNPpACzTtO5OVERUZwh3Q.webp" />
           </div>
         </div>
       </header>
-      <Swiper
+      {/* <Swiper
         direction={'vertical'}
         modules={[Pagination]}
         style={{height : '86%'}}
       >
+        <SwiperSlide className='h-full'> */}
+          <div className='w-fit mx-auto h-[86%]'>
+            <Stories />
+          </div>
+        {/* </SwiperSlide>
         <SwiperSlide className='h-full'>
           <div className='w-fit mx-auto'>
             <Stories />
           </div>
         </SwiperSlide>
-        <SwiperSlide className='h-full'>
-          <div className='w-fit mx-auto'>
-            <Stories />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      </Swiper> */}
       <div className='bg-slate-100 h-[7%] flex items-center'>
-        <div className='  flex justify-between  py-1 px-5 w-full'>
-          <div>
-            <img src={beLL} className='h-8' />
-          </div>
-          <div className='rounded-full overflow-hidden border-2 border-primaryPink'>
-            <img className='h-8' src="https://pics.craiyon.com/2023-11-26/oMNPpACzTtO5OVERUZwh3Q.webp" />
-          </div>
+        <div className='  flex justify-between  py-1 px-5 w-full text-gray-700'>
+          <MdCall className='h-6 w-6' />
+          <FaNoteSticky className='h-6 w-6 '/>
+          <FaSearch className='h-6 w-6'/>
+          <FaPlus className='h-6 w-6'/>
+          <FaShareSquare className='h-6 w-6'/>
         </div>
       </div>
     </main>
--- src/Components/CustomStoryContent.jsx
+++ src/Components/CustomStoryContent.jsx
@@ -4,11 +4,11 @@ import CustomCollapsedComponent from './CustomCollapsedComponent'
 
 const CustomStoryContent = ({ story, action }) => {
   return <WithSeeMore story={story} action={action}>
-    <div>
-      <h1>Hello!</h1>
-      <p>This story would have a 'See More' link at the bottom ✨</p>
-    </div>
-  </WithSeeMore>
+  <div>
+    <h1>Hello!</h1>
+    <p>This story would have a 'See More' link at the bottom ✨</p>
+  </div>
+</WithSeeMore>
 }
 
 export default CustomStoryContent
